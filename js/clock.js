3f3ea8d2bee95eec26bf2d9a149948ec4f44bb2b
Fatrai2 <midgard@citromail.hu>
2020-12-01 16:29:58 +0100

    html

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ setInterval(()=>{
     const hour = document.querySelector("#hour");
     const minutes = document.querySelector("#minutes");
     const seconds = document.querySelector("#seconds");
-    const ampm = document.querySelector("#ampm");
+    
 
     const h = new Date().getHours();
     const m = new Date().getMinutes();
